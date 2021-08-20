eb5ba1a92b23154086747040e51c39e207230eae
muxthafa <musthafamohd0@gmail.com>
2021-08-20 23:48:47 +0530

    Added: program to compute quadratic equation

--- Quadratic.java
+++ Quadratic.java
@@ -3,9 +3,14 @@ package com.corejava;
 import java.util.Scanner;
 
 public class Quadratic {
+	/* quadraticEquation method is used to compute quadratic equation
+	 * @param a is the first parameter
+	 * @param b is the second parameter
+	 * @param c is the third parameter
+	 */
 	public static void quadraticEquation(double a, double b, double c) {
 		double delta = (b*b)-(4*a*c);
-		double x1 = (-b+Math.sqrt(delta))/(2*a);
+		double x1 = (-b+Math.sqrt(delta))/(2*a);		//root formula for the equation
 		double x2 = (-b-Math.sqrt(delta))/(2*a);
 		System.out.println("Root1 of x is : "+x1);
 		System.out.println("Root2 of x is : "+x2);
@@ -15,6 +20,7 @@ public class Quadratic {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter three values :");
+		// a,b,c are input values
 		double a=sc.nextDouble();
 		double b=sc.nextDouble();
 		double c=sc.nextDouble();
