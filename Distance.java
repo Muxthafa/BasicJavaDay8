be8cf800c3850df37b5e69329b1124c1c51cfe63
muxthafa <musthafamohd0@gmail.com>
2021-08-20 23:39:48 +0530

    Added: program to find euclidean distance

--- Distance.java
+++ Distance.java
@@ -1,15 +1,22 @@
 package com.corejava;
 
 public class Distance {
+	/* euclideanDistance method is used to compute euclidean distance
+	 * @param x is the first parameter- x coordinate value
+	 * @param y is the second parameter- y coordinate value
+	 */
 	public static void euclideanDistance(int x, int y){
 		double d=Math.sqrt(Math.pow(x,x)+Math.pow(y,y));
 		System.out.println("Distance is : "+d);
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int x=Integer.parseInt(args[0]);
+		/* Input from command line arguments which stores as string
+		 * converting to Integer using parseint method
+		 */
+		int x=Integer.parseInt(args[0]);	
 		int y=Integer.parseInt(args[1]);
-		euclideanDistance(x,y);
+		euclideanDistance(x,y);		//Function call to euclideanDistance
 	}
 
 }
