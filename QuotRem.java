223d4efea7693c545990d55d147e3f9f430fc9ee
muxthafa <musthafamohd0@gmail.com>
2021-08-20 23:58:03 +0530

    Added: program to find Quotient and reminder

--- QuotRem.java
+++ QuotRem.java
@@ -6,10 +6,10 @@ public class QuotRem {
 	public static void main(String[] args)
 	{
 		Scanner sc =new Scanner(System.in);
-		System.out.println("Enter the dividend:");
-		int num=sc.nextInt();
+		System.out.println("Enter the dividend:");	
+		int num=sc.nextInt();						//Takes dividend value
 		System.out.println("Enter the divisor:");
-		int den=sc.nextInt();
+		int den=sc.nextInt();						//Takes divisor value
 		sc.close();
 		int quotient = num/den;		//gives quotient
 		int reminder = num% den;	//gives reminder
