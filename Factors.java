0bd4c08b8d0072577317619b72dc80b09b392616
muxthafa <musthafamohd0@gmail.com>
2021-08-21 00:00:01 +0530

    Added: program to find prime factors of a number

--- Factors.java
+++ Factors.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class Factors {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Scanner sc=new Scanner(System.in);
 		System.out.println("Enter the number:");	//number to find prime factor
 		int n=sc.nextInt();
