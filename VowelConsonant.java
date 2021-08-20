0c8ce170628eee60125a2b46d5e5a6cb1c3536b4
muxthafa <musthafamohd0@gmail.com>
2021-08-20 23:55:14 +0530

    Added: program to check an alphabet if vowel or not

--- VowelConsonant.java
+++ VowelConsonant.java
@@ -7,7 +7,7 @@ public class VowelConsonant {
 	{
 		Scanner sc =new Scanner(System.in);
 		System.out.println("Enter an alphabet:");
-		char alph = sc.next().charAt(0);		//takes an alphabet to compare with vowels
+		char alph = sc.next().charAt(0);		//Takes an alphabet to compare with vowels
 		if(alph == 'a' || alph == 'A' || alph == 'e' || alph == 'E' || alph == 'i' || alph == 'I' || alph == 'o' || alph == 'O' || alph == 'u' || alph == 'U')
 		{
 			System.out.println("Vowel");
