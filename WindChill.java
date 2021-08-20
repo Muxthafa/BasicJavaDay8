89e5e8ef937bf89ed6d98ef9e671c9d06dacb62c
muxthafa <musthafamohd0@gmail.com>
2021-08-20 23:56:01 +0530

    Added: program to calculate wind chill value

--- WindChill.java
+++ WindChill.java
@@ -1,6 +1,10 @@
 package com.corejava;
 
 public class WindChill {
+	/* windChill method is used to calculate effective temperature
+	 * @param t is the first parameter corresponding to temperature
+	 * @param v is the second parameter corresponding to wind speed
+	 */
 	public static void windChill(int t, int v){
 		double w=((35.74)+(0.6215)*t)+((0.4275)*t-(35.75))*(Math.pow(v,0.16));
 		System.out.println(w);
@@ -8,9 +12,12 @@ public class WindChill {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int t=Integer.parseInt(args[0]);
-		int v=Integer.parseInt(args[1]);
-		windChill(t,v);
+		/*inputs from command line arguments which stores as string
+		 * converting to integer using parseint method
+		 */
+		int t=Integer.parseInt(args[0]);	//temperature in fahrenheit
+		int v=Integer.parseInt(args[1]);	//wind speed in miles/hour
+		windChill(t,v);		//function call to windChill
 	}
 
 }
