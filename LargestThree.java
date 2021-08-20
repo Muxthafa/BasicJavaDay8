ca7fed9e718f57b33f0eb3cc47edd983473ee1a5
muxthafa <musthafamohd0@gmail.com>
2021-08-20 23:59:07 +0530

    Added: program to find largest of three numbers

--- LargestThree.java
+++ LargestThree.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class LargestThree {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Scanner sc =new Scanner(System.in);
 		System.out.println("Enter the first number:");
 		int num1 = sc.nextInt();
