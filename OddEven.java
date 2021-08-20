75e770003f58cc0284ecb1498f0cf9fb7fbd92f9
muxthafa <musthafamohd0@gmail.com>
2021-08-20 23:46:49 +0530

    Added: program to check a number if odd or even

--- OddEven.java
+++ OddEven.java
@@ -7,7 +7,7 @@ public class OddEven {
 	public static void main(String[] args) {
 		Scanner sc =new Scanner(System.in);
 		System.out.println("Enter the number:");
-		int num = sc.nextInt();
+		int num = sc.nextInt();	//enter the number
 		sc.close();
 		if(num%2 == 0)			//dividing value by 2 gives either 0 or 1, if the reminder is 0 then even else odd
 			System.out.println("Even");
