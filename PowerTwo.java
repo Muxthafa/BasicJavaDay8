3e78c41422de73d26fe1567bde8c0b2e2001125b
muxthafa <musthafamohd0@gmail.com>
2021-08-20 23:47:59 +0530

    Added: program to print power of two

--- PowerTwo.java
+++ PowerTwo.java
@@ -5,7 +5,7 @@ public class PowerTwo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int n = Integer.parseInt(args[0]);	//passing command line argument and converting to integer
-		if(n>0 && n<31)
+		if(n>0 && n<31)						//integer must be between 0 and 31
 		{
 			for(int i=0;i<n;i++)
 			{
