c324e2fb0d0d544d91807c8ab4c2d91acd03a2f4
muxthafa <musthafamohd0@gmail.com>
2021-08-20 23:23:07 +0530

    Added:Program to read and display 2D array

--- Array2D.java
+++ Array2D.java
@@ -1,8 +1,14 @@
 package com.corejava;
 
+import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class Array2D {
+	/* readInput method is used to take inputs for m*n matrix
+	 * @param arr is the first parameter
+	 * @param m is the second argument which defines row
+	 * @param n is the third argument which defines column
+	 */
 	public static void readInput(int[][] arr, int m, int n) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the elements of the array : ");
@@ -13,26 +19,40 @@ public class Array2D {
 		}
 		sc.close();
 	}
+	/* printArray method is used to print m*n matrix
+	 * @param arr is the first parameter
+	 * @param m is the second argument which defines row
+	 * @param n is the third argument which defines column
+	 */
 	public static void printArray(int[][] arr, int m, int n) {
-		System.out.println("The elements of the array are : ");
-		for(int i=0;i<m;i++) {
-			for(int j=0;j<n;j++) {
-				System.out.print(arr[i][j]+"\t");
+		System.out.println("Array elements are written into output.txt file:");
+		try {
+      			PrintWriter output = new PrintWriter("output.txt");
+			for(int i=0;i<m;i++)
+			{
+				for(int j=0;j<n;j++)
+					output.print(arr[i][j]+" ");
+				output.print("\n");
 			}
-			System.out.println("");
-		}
+
+      		
+      			output.close();
+    		}
+    		catch(Exception e) {
+      			e.getStackTrace();
+			}
 			
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);	//Java scanner class to take inputs
 		System.out.println("Enter the values of row and columns for a M X N Array : ");
 		int m = sc.nextInt();
 		int n = sc.nextInt();
-		int[][] arr = new int[m][n];
-		readInput(arr,m,n);
-		printArray(arr,m,n);
+		int[][] arr = new int[m][n];	//2D array initialization
+		readInput(arr,m,n);				//function call to readInput
+		printArray(arr,m,n);			//function call to printArray
 		sc.close();
 	}
 
